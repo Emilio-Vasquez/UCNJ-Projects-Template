d0c6b894cc0909bf2fc1e28da615f9c9f90e97e9
Emilio-Vasquez <emiliovasquezcarbajalalexander@gmail.com>
2025-06-26 09:51:03 -0400

    added js code, changed gitignore, changed template to replicate the demo, added styling and included route in app.py

--- Template/static/js/script.js
+++ Template/static/js/script.js
@@ -3,6 +3,7 @@
 console.log("JavaScript is linked and working!");
 
 // Example: DOM interaction
+// All this script does is change the color of the header 2 into blue
 document.addEventListener("DOMContentLoaded", function() {
     const header = document.querySelector("h2");
     if (header) {
